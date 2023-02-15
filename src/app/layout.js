3af929284917ce27460a3c3804e16a459c7b8a21
UJustBe Tech <88655561+ujustbetech@users.noreply.github.com>
2023-02-15 12:01:26 +0530

    Files without css

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,13 +4,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       {/*
-         will contain the components returned by the nearest parent
+        <head /> will contain the components returned by the nearest parent
         head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head>
-        <link href='https://fonts.googleapis.com/css2?family=Mukta&display=swap'></link>
-        <link href='https://fonts.googleapis.com/css2?family=Mukta&display=swaphttps://fonts.googleapis.com/css2?family=Forum&display=swap'></link>
-      </head>
+      <head />
       <body>{children}</body>
     </html>
   )
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 // import Image from 'next/image'
 // import { Inter } from '@next/font/google'
 // import styles from './page.module.css'
-import Dashboard from '../components/Dashboard'
-import Login from '../components/Login'
-import styles from '../styles/main.css'
+// import Dashboard from '../components/Dashboard'
+// import Login from '../components/Login'
+// import styles from '../styles/main.scss'
+
+import Login from "../component/Login";
 
 
 // const inter = Inter({ subsets: ['latin'] })
@@ -11,7 +13,10 @@ import styles from '../styles/main.css'
 export default function Home() {
   return (
     <>
+      {/* <Login/> */}
       <Login/>
+      {/* <h1>anniversary page</h1>
+      <h6>Content</h6> */}
     </>
   )
 }
